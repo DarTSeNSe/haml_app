45de5c62ea1d3268677b78284e8617b5cc960c69
Alex SeNSe <sense8life@gmail.com>
2011-12-20 17:25:14 +0200

    haml

---
+++ app/controllers/visit_controller.rb
@@ -0,0 +1,5 @@
+class VisitController < ApplicationController
+  def show
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamlApp::Application.config.secret_token = 'e15107a00af75c35b8e92151b71f5bdfbfb43f4f26f3cca27ac8889b4b774db4a51c70f8e4521496d11e855738808ea3295a48e164dfd4b7ca61fe18766beb10'
